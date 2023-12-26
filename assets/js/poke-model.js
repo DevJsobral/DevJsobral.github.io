3fde21332351c9e999b381fef86d8017a0cb5d85
DevJsobral <109977062+DevJsobral@users.noreply.github.com>
2023-12-26 15:28:56 -0300

    Add files via upload

---
+++ assets/js/poke-model.js
@@ -0,0 +1,21 @@
+
+class Pokemon {
+    number;
+    name;
+    type;
+    types = [];
+    photo;
+    height;
+    weight;
+    abilities = [];
+    gender;
+    egg = [];
+    hp;
+    atk;
+    def;
+    spatk;
+    spdef;
+    speed;
+    total;
+}
+
